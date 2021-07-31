af5892d89972ff939abb441e3911da83257b7118
zakaria <e-zk@users.noreply.github.com>
2021-07-31 18:42:06 +1000

    Update terminal module
    - Remove old, unused cruft
    - Don't read single char - wait for newline

--- term/term.go
+++ term/term.go
@@ -4,48 +4,18 @@ package term
 
 import (
 	"fmt"
-	t "golang.org/x/crypto/ssh/terminal"
 	"os"
-)
-
-const (
-	passwdPrompt = "secret: "
 )
 
 // ask a y/n question. get a boolean back
 func Ask(message string) (resp bool, err error) {
-	fmt.Fprintf(os.Stderr, "%s ", message)
+	var in string
+	fmt.Fprintf(os.Stderr, "%s [y/N] ", message)
+	fmt.Scanf("%s", &in)
 
-	// raw terminal
-	oldstate, err := t.MakeRaw(0)
-	if err != nil {
-		return false, err
-	}
-
-	// read single char
-	var in []byte = make([]byte, 1)
-	os.Stdin.Read(in)
-
-	// restore terminal
-	t.Restore(0, oldstate)
-	fmt.Fprintf(os.Stderr, "\n")
-
-	if !(string(in[0]) == "y" || string(in[0]) == "Y") {
+	if !(in[0] == 'y' || in[0] == 'Y') {
 		return false, nil
 	}
 
 	return true, nil
-}
-
-// ask for the password
-func AskPasswd() (secret []byte, err error) {
-	fmt.Fprintf(os.Stderr, passwdPrompt)
-
-	secret, err = t.ReadPassword(0)
-	if err != nil {
-		return nil, err
-	}
-
-	fmt.Fprintf(os.Stderr, "\n")
-	return secret, nil
 }
